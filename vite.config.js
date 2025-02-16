eef6f7ef9993d0478eca652de8c9bd60d2ff0720
iamamansid <56902850+iamamansid@users.noreply.github.com>
2025-02-16 11:26:00 +0530

    vite change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/doc-ocr-ui/', // Add this line
   plugins: [react()],
-})
+});
