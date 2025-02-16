2f0ccbfdd9c37af2e907cd597c34ffb7a10363cf
iamamansid <56902850+iamamansid@users.noreply.github.com>
2025-02-16 11:38:43 +0530

    vite fixes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   base: '/doc-ocr-ui/', // Add this line
   plugins: [react()],
+  build: {
+      outDir: "dist",
+    },
 });
