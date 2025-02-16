bf4bc5c38d0782cabaa9d9dc45b0b93c3f94aab1
iamamansid <56902850+iamamansid@users.noreply.github.com>
2025-02-16 13:11:39 +0530

    Stable version

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client'; // Import createRoot from react-dom/client
-import App from './App';
+import App from './App.jsx';
 
 // Create a root element
-const container = document.getElementById('root');
-const root = createRoot(container);
+// const container = document.getElementById('root');
+// const root = createRoot(container);
 
 // Render the app
-root.render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
